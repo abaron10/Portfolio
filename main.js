0c9e7803abc958ecc067aec0a5360ae2a8ba44b2
andres.baron@mercadolibre.com.co <af.baron10@uniandes.edu.co>
2021-07-18 16:30:03 -0500

    initial

---
+++ main.js
@@ -0,0 +1,10 @@
+function articles(){
+    window.location.href="https://af-baron10.medium.com/";
+}
+
+
+
+function contact(){
+    document.getElementById("subtitle-contact").scrollIntoView({behavior: 'smooth'});
+
+}
