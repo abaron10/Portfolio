3a7f03b4e890a9e0a377b996ad3b035c820630dd
andres.baron@mercadolibre.com.co <af.baron10@uniandes.edu.co>
2021-07-18 17:56:20 -0500

    Images added!

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 function articles(){
     window.location.href="https://af-baron10.medium.com/";
 }
+function linkedin(){
+    window.location.href="https://www.linkedin.com/in/andres-baron-sandoval-76ab96186/";
+}
+function git(){
+    window.location.href="https://github.com/abaron10";
+}
 
+function download(){
+    window.open("./cves.pdf", '_self');
+}
 
 
 function contact(){
