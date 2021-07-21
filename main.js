3a5bc99e7f079e2084463f23598ba8be5ec6a957
andres.baron@mercadolibre.com.co <af.baron10@uniandes.edu.co>
2021-07-21 16:49:54 -0500

    pictures added

--- main.js
+++ main.js
@@ -17,3 +17,22 @@ function contact(){
     document.getElementById("subtitle-contact").scrollIntoView({behavior: 'smooth'});
 
 }
+
+let textLength = 0;
+let text = "Full Stack developer & Master in Software Engineering Student";
+
+function type() {
+    let textChar = text.charAt(textLength++);
+    let paragraph = document.getElementById("typed");
+    let charElement = document.createTextNode(textChar);
+    paragraph.appendChild(charElement);
+    if(textLength < text.length+1) {
+        setTimeout('type()', 50);
+    } else {
+        text = 'Full Stack developer & Master in Software Engineering Student';
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    type();
+});
